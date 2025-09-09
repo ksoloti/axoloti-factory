07f37aed43c6ebc18a4e0a6b56d6bebfb0cff1b0
sebiik <sebiik@gmail.com>
2025-09-09 17:57:18 +0800

    Include loadElementsData.h locally

--- objects/fx/lmnts/loadElementsData.h
+++ objects/fx/lmnts/loadElementsData.h
@@ -6,7 +6,11 @@ extern "C" bool loadElementsData(int idx, const char* fn, int16_t sample_array[]
     FRESULT err;
     UINT bytes_read;
     int i;
-    if (idx>1) {LogTextMessage("Elements sample array index must be < 2"); return false;}
+
+    if (idx>1) {
+        LogTextMessage("Elements sample array index must be < 2");
+        return false;
+    }
 
     err = f_open(&FileObject, fn, FA_READ | FA_OPEN_EXISTING);
     if (err != FR_OK) {
@@ -17,10 +21,17 @@ extern "C" bool loadElementsData(int idx, const char* fn, int16_t sample_array[]
         }
         return false;
     }
+
     err = f_read(&FileObject, sample_array, size * 2, &bytes_read);
-    if (err != FR_OK) {LogTextMessage("Read failed %s\n", fn); return false;}
+    if (err != FR_OK) {
+        LogTextMessage("Read failed %s\n", fn);
+        return false;
+    }
     err = f_close(&FileObject);
-    if (err != FR_OK) {LogTextMessage("Close failed %s\n", fn); return false;}
+    if (err != FR_OK) {
+        LogTextMessage("Close failed %s\n", fn);
+        return false;
+    }
 
     LogTextMessage("Bytes Read %s, %d\n", fn, bytes_read);
     i = bytes_read / 2; // 16 bit per sample
